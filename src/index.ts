6dd3e38fdc183e1fb14cebad2c7c5420f1ce4d08
SUHO PARK <duglikespiano@gmail.com>
2026-08-16 13:53:06 +0900

    REFACTOR - QUERY STRING RELATED LOGIC REFACTORED

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,31 @@ const app: Express = express();
 const server = http.createServer(app);
 const PORT = 8000;
 
+type PetQueryParams = {
+	species?: string;
+	adopted?: 'true' | 'false';
+	minAge?: string;
+	maxAge: string;
+};
+
 app.use(cors());
 
-app.get('/', (req: Request, res: Response<Pet[]>): void => {
-	res.json(pets);
+app.get('/', (req: Request<{}, unknown, {}, PetQueryParams>, res: Response<Pet[]>): void => {
+	const { species, adopted, minAge, maxAge } = req.query;
+	let filteredPets: Pet[] = pets;
+	if (species) {
+		filteredPets = filteredPets.filter((pet: Pet): boolean => pet.species.toLowerCase() === species.toLowerCase());
+	}
+	if (adopted) {
+		filteredPets = filteredPets.filter((pet: Pet): boolean => pet.adopted === JSON.parse(adopted));
+	}
+	if (minAge) {
+		filteredPets = filteredPets.filter((pet: Pet): boolean => pet.age >= JSON.parse(minAge));
+	}
+	if (maxAge) {
+		filteredPets = filteredPets.filter((pet: Pet): boolean => pet.age <= JSON.parse(maxAge));
+	}
+	res.json(filteredPets);
 });
 
 app.get('/:id', (req: Request<{ id: string }>, res: Response<Pet | { message: string }>): void => {
