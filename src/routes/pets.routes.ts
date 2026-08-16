4d34d179550e30157eec370cb7e92d8b1163b95e
SUHO PARK <duglikespiano@gmail.com>
2026-08-17 00:15:50 +0900

    MIDDLEWARE - MIDDLEWARE ADDED

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { getPets, getPetById } from '../controllers/pets.controllers';
+import { validateNumericId, pleaseAuth } from '../middlewares/pets.middleware';
 import type { Router } from 'express';
 
 export const petRouter: Router = express.Router();
 
 petRouter.get('/', getPets);
-petRouter.get('/:id', getPetById);
+petRouter.get('/:id', pleaseAuth, validateNumericId, getPetById);
